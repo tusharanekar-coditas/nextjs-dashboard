22ee59b123a237138fc77c9f0c7ec9fd9fb0006e
tusharanekar-coditas <tushar.anekar@coditas.com>
2025-11-14 17:15:24 +0530

    feat: test

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -43,6 +43,9 @@ export function Card({
 
   return (
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
+      <p>
+        {title}
+      </p>
       <div className="flex p-4">
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
